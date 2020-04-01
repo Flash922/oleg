dd18cfd0ecc6b83a02e6387eb7af83781dbd4569
Flash922 <Flash92@gmail.com>
2020-04-01 10:14:18 +0300

    complite  section m-slider

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,40 @@
   //   });
   // });
 
-  $('.m-slider__box').slick({
-    rtl: true
+
+
+
+    // var $status = $('.pagingInfo');
+    // var $slickElement = $('.m-slider__box');
+
+    // $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //     //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    //     var i = (currentSlide ? currentSlide : 0) + 1;
+    //     $status.text(i + '/' + slick.slideCount);
+    // });
+
+    // $slickElement.slick({
+    //     slide: 'slide',
+    //     // slidesToShow:1,
+    //     // slidesToScroll:1,
+    //     autoplay: false,
+    //     dots: true
+    // });
+    var $status = $('.snav__paging-info');
+    var $slickElement = $('.m-slider__box');
+
+    $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
+      //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+      var i = (currentSlide ? currentSlide : 0) + 1;
+      $status.text(i + '/' + slick.slideCount);
+    });
+    $slickElement.slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
+      arrows: true,
+      dots: true,
+      appendDots: $('.snav__dots-list'),
+      nextArrow: document.querySelector('.snav__next'),
+      prevArrow: document.querySelector('.snav__prev'),
   });
