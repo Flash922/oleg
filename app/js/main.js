f4a483da5bd8a356b40ca93d7bf2abc5abff5b52
Flash922 <Flash92@gmail.com>
2020-04-04 19:06:23 +0300

    fixed 2 slider

--- app/js/main.js
+++ app/js/main.js
@@ -19,39 +19,66 @@
   // });
 
 
+//start slider section m-slider
+      $(document).ready(function(){
 
+      //this vars add page number for slider
+      var $status = $('.snav__paging-info');
+      var $slickElement = $('.m-slider__box');
 
-    // var $status = $('.pagingInfo');
-    // var $slickElement = $('.m-slider__box');
+      //this vars add page number for slider
+      $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+        //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $status.text(i + '/' + slick.slideCount);
+      });
+      //this body slider
+      $slickElement.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: true,
+        dots: true,
 
-    // $slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //     //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-    //     var i = (currentSlide ? currentSlide : 0) + 1;
-    //     $status.text(i + '/' + slick.slideCount);
-    // });
+        // add customs dost from class html .snav__dots-list
+        appendDots: $('.snav__dots-list'),
 
-    // $slickElement.slick({
-    //     slide: 'slide',
-    //     // slidesToShow:1,
-    //     // slidesToScroll:1,
-    //     autoplay: false,
-    //     dots: true
-    // });
-    var $status = $('.snav__paging-info');
-    var $slickElement = $('.m-slider__box');
+        //add customs arrows from class html .snav__next and .snav__prev
+        nextArrow: document.querySelector('.snav__prev'),
+        prevArrow: document.querySelector('.snav__next'),
+    });
+  });
+  //end slider section m-slider
 
-    $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide) {
+  
+  //start slider section portfolio
+  $(document).ready(function(){
+    var $status = $('.pnav__paging-info');
+    var $slickElement = $('.p-slider__box');
+
+  //thid remove button and numbre dots and add class for dots
+    $slickElement.on('init', function(event, slick){
+      var $items = slick.$dots.find('li');
+      $items.addClass('pnav__dots');
+      $items.find('button').remove();
+      });
+
+    //this vars add page number for slider
+    $slickElement.on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
       //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
       var i = (currentSlide ? currentSlide : 0) + 1;
       $status.text(i + '/' + slick.slideCount);
     });
+
+  
     $slickElement.slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      fade: true,
-      arrows: true,
-      dots: true,
-      appendDots: $('.snav__dots-list'),
-      nextArrow: document.querySelector('.snav__next'),
-      prevArrow: document.querySelector('.snav__prev'),
-  });
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true,
+        // add customs dost from class html .snav__dots-list
+        appendDots: $('.pnav'),
+    });
+});
+//end slider section portfolio
